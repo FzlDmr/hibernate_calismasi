86e9a1c3f237ff431bcad645329aae3468d785d3
Aynur Ahtıvar <aynurahtivar@gmail.com>
2023-08-31 14:19:04 +0300

    deneme3

--- src/main/java/hibernatequerylanguage/deneme.java
+++ src/main/java/hibernatequerylanguage/deneme.java
@@ -4,5 +4,9 @@ public class deneme {
 
     public static void main(String[] args) {
         System.out.println("Aynur deneme");
+        System.out.println("deneme222");
+        System.out.println("deneme222");
+        System.out.println("deneme222");
+        System.out.println("deneme222");
     }
 }
